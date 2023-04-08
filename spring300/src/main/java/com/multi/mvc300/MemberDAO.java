2a0035f9e5f3719f7cf5cd964849229504cc8a62
hkchae3270 <124110985+hkchae3270@users.noreply.github.com>
2023-04-08 14:24:28 +0900

    Add files via upload

---
+++ spring300/src/main/java/com/multi/mvc300/MemberDAO.java
@@ -0,0 +1,51 @@
+package com.multi.mvc300;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.Locale;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MemberDAO { // CRUD
+//특정한 방법으로 실행하는 클래스(model, 방법) 
+//	public int login(MemberVO bag) {
+//		return result;
+//	}
+//
+//	public ArrayList<MemberVO> list() {
+//		return list;
+//	}
+//
+//	// 메서드를 만드는 것 ==> 메서드 정의(define)!
+//	// 메서드를 정의했다고 실행되는 것은 아니다.!
+//	// 메서드를 쓰는 것 ==> 메서드 호출(call)!
+//	public MemberVO one(String id) {
+//		
+//		return bag;
+//
+//	}
+//
+//	public int delete(String id) {
+//		
+//
+//		return result;
+//
+//	}
+//
+//	// 1. 가방을 받아서 저장해두자.
+//	// 2. 필요할 때 가방에서 값들을 하나씩 꺼내자.
+//	public int update(MemberVO bag) {
+//		
+//		return result;
+//	}
+
+	// public void add2() {
+	public int insert(MemberVO bag) {
+		
+		return result;
+	}
+}
