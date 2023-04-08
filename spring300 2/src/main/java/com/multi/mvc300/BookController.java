59ded2d52b90462b41e6068353675ee9e246e2c3
hkchae3270 <124110985+hkchae3270@users.noreply.github.com>
2023-04-09 02:13:48 +0900

    Add files via upload

---
+++ spring300 2/src/main/java/com/multi/mvc300/BookController.java
@@ -0,0 +1,24 @@
+package com.multi.mvc300;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BookController {
+	
+	@Autowired
+	BookDAO dao;//100
+	
+	@RequestMapping("insert3.multi")
+	public void insert(BookVO bag) {
+		
+		System.out.println("insert요청됨.");
+		System.out.println(bag);
+		System.out.println(dao);
+		dao.insert(bag);
+	}
+}
